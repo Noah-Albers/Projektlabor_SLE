ffacb3f72f092578505584aea4c76d9c8452ecd5
Francis <francis@non-existant.com>
2022-03-16 17:56:24 +0100

    website erstellt

---
+++ scr.js
@@ -0,0 +1,36 @@
+var toolbox = {
+    "kind": "flyoutToolbox",
+    "contents": [
+      {
+        "kind": "block",
+        "type": "controls_if"
+      },
+      {
+        "kind": "block",
+        "type": "controls_repeat_ext"
+      },
+      {
+        "kind": "block",
+        "type": "logic_compare"
+      },
+      {
+        "kind": "block",
+        "type": "math_number"
+      },
+      {
+        "kind": "block",
+        "type": "math_arithmetic"
+      },
+      {
+        "kind": "block",
+        "type": "text"
+      },
+      {
+        "kind": "block",
+        "type": "text_print"
+      },
+    ]
+  }
+window.onload=function(){
+    var workspace=Blockly.inject("blocklydiv", {toolbox: toolbox});
+}
